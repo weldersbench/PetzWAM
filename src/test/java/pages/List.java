a3dd5c393f82e3867d41e1a3cb66a67f5152f112
weldersbench <leandro.h1512@gmail.com>
2021-03-22 22:35:57 -0300

    Criamos os mapeamentos das pag List e Product e a feature Search a Product.

--- src/test/java/pages/List.java
+++ src/test/java/pages/List.java
@@ -1,4 +1,30 @@
 package pages;
 
-public class List {
+import general.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class List extends Base {
+
+    @FindBy(css = "h1.h2Categoria.nomeCategoria")
+    private WebElement lblResultBy;
+
+
+    public List(WebDriver driver){
+        super(driver);
+    }
+
+    public String readTitleTab(){
+        return driver.getTitle();
+    }
+
+    public String readResultBy(){
+        return lblResultBy.getText();
+    }
+
+    public void clickAtDesiredProduct(String product){
+        driver.findElement(By.xpath("//h3[contains(text(),'" + product + "')]")).click();
+    }
 }
--- src/test/java/pages/Product.java
+++ src/test/java/pages/Product.java
@@ -1,4 +1,31 @@
 package pages;
 
-public class Product {
+import general.Base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Product extends Base {
+
+    @FindBy(css = "div.col-md-7.prod-info:nth-child(6) > h1:nth-child(1)")
+    private WebElement productTitle;
+
+    @FindBy(css = "div.price-current")
+    private WebElement priceCurrent;
+
+    public Product(WebDriver driver){
+        super(driver);
+    }
+
+    public String readTitleTab(){
+        return driver.getTitle();
+    }
+
+    public String readProductTitle(){
+        return productTitle.getText();
+    }
+
+    public String readPriceCurrent(){
+        return priceCurrent.getText();
+    }
 }
